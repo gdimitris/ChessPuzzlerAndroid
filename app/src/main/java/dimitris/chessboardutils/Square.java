7edb937fef8af8d007325cd6f9def2b8c41b07cc
gdimitris <dimitrios.yiannakos@gmail.com>
2015-08-30 19:26:24 +0300

    Added Square class

---
+++ app/src/main/java/dimitris/chessboardutils/Square.java
@@ -0,0 +1,15 @@
+package dimitris.chessboardutils;
+
+/**
+ * Created by dimitris on 3/30/15.
+ */
+public class Square {
+
+    public String name;
+    public Piece piece;
+
+    public Square(String name) {
+        this.name = name;
+        this.piece = new NullPiece();
+    }
+}
