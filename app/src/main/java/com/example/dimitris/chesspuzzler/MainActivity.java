cdbb0ad5b08edebbdfda3d2528f03a183ffe49d4
gdimitris <dimitrios.yiannakos@gmail.com>
2015-08-30 14:53:11 +0300

    Adds Robolectric support for Unit tests

--- app/src/main/java/com/example/dimitris/chesspuzzler/MainActivity.java
+++ app/src/main/java/com/example/dimitris/chesspuzzler/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
 public class MainActivity extends Activity {
 
     @Override
---
+++ app/src/test/java/FenParserTests.java
@@ -0,0 +1,31 @@
+import android.os.Build;
+
+import com.example.dimitris.chesspuzzler.BuildConfig;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by dimitris on 8/30/15.
+ */
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
+public class FenParserTests {
+
+    @Before
+    public void setup(){
+
+    }
+
+    @Test
+    public void testThatItWorks(){
+        assertTrue(true);
+    }
+}
