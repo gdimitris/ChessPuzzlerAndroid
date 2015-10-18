713c273e2d1a4f5321fc0eac728987689366b862
gdimitris <dimitrios.yiannakos@gmail.com>
2015-10-18 20:11:11 +0300

    Future tests

--- app/src/main/java/com/example/dimitris/chesspuzzler/ChessBoard.java
+++ app/src/main/java/com/example/dimitris/chesspuzzler/ChessBoard.java
@@ -16,28 +16,31 @@ import com.envious.chesscoach.UserInterface.Pieces.WhitePieceFactory;
 
 import java.util.ArrayList;
 
+import dimitris.chessboardutils.BoardFactory;
+import dimitris.chessboardutils.Square;
+
 import static com.envious.chesscoach.UserInterface.BoardHelper.BoardCoords;
 import static com.envious.chesscoach.UserInterface.BoardHelper.getSquareCoords;
 
-public class ChessBoard extends MoveSubject {
+public class ChessBoard {
 
     private int squareSize;
     private Square lastSelectedSquare;
     private Square[][] board;
-    private MoveExecutor moveExecutor;
-    private MoveFilterer moveFilterer;
+//    private MoveExecutor moveExecutor;
+//    private MoveFilterer moveFilterer;
 
     public ChessBoard(int squareSize) {
         this.squareSize = squareSize;
-        moveObservers = new ArrayList<MoveObserver>();
-        moveExecutor = new MoveExecutor(this);
-        board = new BoardFactory(squareSize).createBoard();
+//        moveObservers = new ArrayList<MoveObserver>();
+//        moveExecutor = new MoveExecutor(this);
+//        board = new BoardFactory(squareSize).createBoard();
 
-        TurnArbiter arbiter = new TurnArbiter(this);
-        moveFilterer = new MoveFilterer();
-        moveFilterer.addFilter(arbiter);
-        moveFilterer.addFilter(new KingCaptureFilter());
-        moveFilterer.addFilter(new SelfCaptureFilter());
+//        TurnArbiter arbiter = new TurnArbiter(this);
+//        moveFilterer = new MoveFilterer();
+//        moveFilterer.addFilter(arbiter);
+//        moveFilterer.addFilter(new KingCaptureFilter());
+//        moveFilterer.addFilter(new SelfCaptureFilter());
     }
 
     public void draw(Canvas canvas) {
@@ -49,94 +52,94 @@ public class ChessBoard extends MoveSubject {
                 board[row][col].draw(canvas);
     }
 
-    protected void squareSelectedAt(int row, int col) {
-
-        if (lastSelectedSquare == null) {
-            selectSquareIfNotEmpty(row, col);
-        } else if (lastSelectedSquare == board[row][col]) {
-            clearSelection();
-        } else {
-            dispatchNewMoveIfPassesFilters(row, col);
-            clearSelection();
-        }
-
-    }
-
-    private void dispatchNewMoveIfPassesFilters(int row, int col) {
-        Move toMake = new Move(lastSelectedSquare, board[row][col]);
-
-        if (moveFilterer.moveIsLegal(toMake))
-            doMove(toMake);
-    }
-
-    private void selectSquareIfNotEmpty(int row, int col) {
-        if (!squareIsEmpty(row, col)) {
-            lastSelectedSquare = board[row][col];
-            lastSelectedSquare.setSelected(true);
-        }
-    }
-
-    private boolean squareIsEmpty(int row, int col) {
-        return board[row][col].getPiece() == null;
-    }
-
-    private void clearSelection() {
-        lastSelectedSquare.setSelected(false);
-        lastSelectedSquare = null;
-    }
-
-    public void setUpPosition(String FEN, Typeface font) {
-        FenParser parser = new FenParser(board, new WhitePieceFactory(font, squareSize), new BlackPieceFactory(font, squareSize));
-        try {
-            parser.parse(FEN);
-        } catch (FenParser.BadFenException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void doMove(Move toDo) {
-        broadcastNewMoveToObservers(toDo);
-    }
-
-    public void undoMove() {
-        broadcastUndoToObservers();
-    }
-
-    public void redoMove() {
-        broadcastRedoToObservers();
-    }
-
-    public Square getSquare(String squareName) {
-        return getSquare(getSquareCoords(squareName));
-    }
-
-    public Square getSquare(BoardCoords coords) {
-        return board[coords.row][coords.column];
-    }
-
-    public BoardCoords getBoardCoordsOfSquare(String squareName) {
-        return getSquareCoords(squareName);
-    }
-
-    public String printMoves() {
-        return moveExecutor.printMoves();
-    }
-
-    @Override
-    public void broadcastUndoToObservers() {
-        for (MoveObserver o : moveObservers)
-            o.undoMove();
-    }
-
-    @Override
-    public void broadcastRedoToObservers() {
-        for (MoveObserver o : moveObservers)
-            o.redoMove();
-    }
-
-    @Override
-    public void broadcastNewMoveToObservers(Move move) {
-        for (MoveObserver o : moveObservers)
-            o.doMove(move);
-    }
+//    protected void squareSelectedAt(int row, int col) {
+//
+//        if (lastSelectedSquare == null) {
+//            selectSquareIfNotEmpty(row, col);
+//        } else if (lastSelectedSquare == board[row][col]) {
+//            clearSelection();
+//        } else {
+//            dispatchNewMoveIfPassesFilters(row, col);
+//            clearSelection();
+//        }
+//
+//    }
+//
+//    private void dispatchNewMoveIfPassesFilters(int row, int col) {
+//        Move toMake = new Move(lastSelectedSquare, board[row][col]);
+//
+//        if (moveFilterer.moveIsLegal(toMake))
+//            doMove(toMake);
+//    }
+//
+//    private void selectSquareIfNotEmpty(int row, int col) {
+//        if (!squareIsEmpty(row, col)) {
+//            lastSelectedSquare = board[row][col];
+//            lastSelectedSquare.setSelected(true);
+//        }
+//    }
+//
+//    private boolean squareIsEmpty(int row, int col) {
+//        return board[row][col].getPiece() == null;
+//    }
+//
+//    private void clearSelection() {
+//        lastSelectedSquare.setSelected(false);
+//        lastSelectedSquare = null;
+//    }
+//
+//    public void setUpPosition(String FEN, Typeface font) {
+//        FenParser parser = new FenParser(board, new WhitePieceFactory(font, squareSize), new BlackPieceFactory(font, squareSize));
+//        try {
+//            parser.parse(FEN);
+//        } catch (FenParser.BadFenException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public void doMove(Move toDo) {
+//        broadcastNewMoveToObservers(toDo);
+//    }
+//
+//    public void undoMove() {
+//        broadcastUndoToObservers();
+//    }
+//
+//    public void redoMove() {
+//        broadcastRedoToObservers();
+//    }
+//
+//    public Square getSquare(String squareName) {
+//        return getSquare(getSquareCoords(squareName));
+//    }
+//
+//    public Square getSquare(BoardCoords coords) {
+//        return board[coords.row][coords.column];
+//    }
+//
+//    public BoardCoords getBoardCoordsOfSquare(String squareName) {
+//        return getSquareCoords(squareName);
+//    }
+//
+//    public String printMoves() {
+//        return moveExecutor.printMoves();
+//    }
+//
+//    @Override
+//    public void broadcastUndoToObservers() {
+//        for (MoveObserver o : moveObservers)
+//            o.undoMove();
+//    }
+//
+//    @Override
+//    public void broadcastRedoToObservers() {
+//        for (MoveObserver o : moveObservers)
+//            o.redoMove();
+//    }
+//
+//    @Override
+//    public void broadcastNewMoveToObservers(Move move) {
+//        for (MoveObserver o : moveObservers)
+//            o.doMove(move);
+//    }
 }
